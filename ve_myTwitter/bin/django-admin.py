2e8ea22ba25f6f1a6abd3463192d4fc08829dd28
MagdaZawora <magda.zawora@gmail.com>
2017-09-16 08:43:47 +0200

    Removed folder from directory

--- ve_myTwitter/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/magdazawora/workspace/Django/myTwitter/ve_myTwitter/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
